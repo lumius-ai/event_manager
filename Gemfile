5ee84bf7af134764cfc5c6e7d79049648f7c9a02
Razvan Rotundu <raz.rotundu@gmail.com>
2024-10-05 12:59:37 -0400

    Zipcode cleaning and Civicinfo API usage
    move zipcode cleaning code into functions clean_zipcode and clean_zipcode_2
    add google/apis/civicinfo_v2 to bundle
    add call to google CivicinfoV2 API to get legislator info by zipcode
    add api key in secret.key
    add secret.key to gitignore

--- Gemfile
+++ Gemfile
@@ -5,3 +5,5 @@ source "https://rubygems.org"
 # gem "rails"
 
 gem "rspec", "~> 3.13"
+
+gem "google-apis-civicinfo_v2", "~> 0.24.0"
--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,14 +1,68 @@
 require 'csv'
+require 'google/apis/civicinfo_v2'
 
 # CSVs we're working with
 small = "event_attendees.csv"
 large = "event_attendees_full.csv"
 
+
+
+# Iteration 1, parsing csv using the CSV library
+def one_iteration(infile)
+
+  # Civic info API
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new()
+  civic_info.key = File.read('secret.key').strip
+
+
+  content = CSV.open(infile, 
+  headers: true,
+  header_converters: :symbol
+  )
+  content.each do |row|
+    name = row[:first_name]
+    # Zipcode handling
+    zipcode = clean_zipcode_2(row[:zipcode])
+
+    begin
+      # Get each person's local representative
+      legislators = civic_info.representative_info_by_address(
+        address: zipcode,
+        levels: 'country',
+        roles: ['legislatorUpperBody', 'legislatorLowerBody']
+      )
+      legislators = legislators.officials
+    rescue
+      return 'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials\n'
+    end
+      puts "#{name} #{zipcode} #{legislators}"
+  end
+end
+
+# Zipcode cleaning method 
+def clean_zipcode(zipcode)
+  # Handling incorrect length
+  if zipcode.nil?
+    zipcode = '00000'
+  elsif zipcode.length < 5
+    zipcode = zipcode.rjust(5, '0')
+  elsif zipcode.length > 5
+    zipcode = zipcode[0..4]
+  end
+  return zipcode
+end
+
+# Zipcode cleaner 2 (more succinct) coercion over questions
+def clean_zipcode_2(zipcode)
+  zipcode.to_s.rjust(5, '0')[0..4]
+end
+
 # Iteration 0, manually reading through a csv file
 def zero_iteration(infile)
-  File.exist?(infile) ? content = File.read(infile) : puts("File #{infile} not found in this directory") && return
+  # Basic file opening
+  # File.exist?(infile) ? content = File.read(infile) : puts("File #{infile} not found in this directory") && return
 
-  lines = File.readlines(infile)
+  File.exist?(infile) ? lines = File.readlines(infile): puts("File #{infile} not found in this directory") && return
   lines.each_with_index do |line, index|
     next if index == 0
     columns = line.split(',')
@@ -16,16 +70,5 @@ def zero_iteration(infile)
   end
 end
 
-# Iteration 1, parsing csv using the CSV library
-def one_iteration(infile)
-  content = CSV.open(infile, 
-  headers: true,
-  header_converters: :symbol
-  )
-  content.each do |row|
-    name = row[:first_name]
-    puts name
-  end
-end
 
 one_iteration(small)
